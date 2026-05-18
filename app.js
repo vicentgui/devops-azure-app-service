dc6da283b94099800f8b86c2faf81f1ace053358
Guilherme Vicente <vicentegui01@gmail.com>
2026-05-18 20:35:31 -0300

    Update message for automatic deploy test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
 app.get('/', (req, res) => {
-res.send('Hello from DevOps with Azure App Service!');
+res.send('Hello again from DevOps!');
 });
 app.listen(port, () => {
 console.log(`App listening on port ${port}`);
